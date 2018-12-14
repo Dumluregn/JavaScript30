ee8f201a7c9213021ee3d4cc165a211aae869c01
dumluregn <dumluregn@gmail.com>
2018-12-14 14:07:59 +0100

    Project 11 added fullscreen button and changed cursor over progress bar

--- 11 - Custom Video Player/scripts-BY-MYSELF.js
+++ 11 - Custom Video Player/scripts-BY-MYSELF.js
@@ -5,9 +5,12 @@ const PROGRESSBAR = PLAYER.querySelector(".progress__filled");
 const TOGGLE = PLAYER.querySelector(".toggle");
 const SKIPBUTTONS = PLAYER.querySelectorAll("[data-skip]");
 const RANGES = PLAYER.querySelectorAll(".player__slider");
+const FULLSCREENBUTTON = PLAYER.querySelector(".fullscreen");
 
 PROGRESSBAR.style.flexBasis = "0%";
 let mouseDown = false;
+let fullScreen = false;
+const defaultWidth = "750px";
 
 function togglePlay(){
     VIDEO.paused ? VIDEO.play() : VIDEO.pause();
@@ -35,6 +38,18 @@ function scrub(e){
     VIDEO.currentTime = (e.offsetX / PROGRESS.offsetWidth) * VIDEO.duration;
 }
 
+function toggleFullScreen(){
+    if (fullScreen){
+        PLAYER.style.maxWidth = defaultWidth;
+        PLAYER.style.width = defaultWidth;
+
+    } else {
+        PLAYER.style.maxWidth = window.outerWidth + "px";
+        PLAYER.style.width = window.outerWidth + "px";
+    }
+    fullScreen = !fullScreen;
+}
+
 VIDEO.addEventListener("click", togglePlay);
 VIDEO.addEventListener("play", togglePlayButton);
 VIDEO.addEventListener("pause", togglePlayButton);
@@ -43,6 +58,7 @@ VIDEO.addEventListener("timeupdate", handleProgress);
 TOGGLE.addEventListener("click", togglePlay);
 SKIPBUTTONS.forEach(button => button.addEventListener("click", skip));
 RANGES.forEach(range => range.addEventListener("change", handleRangeUpdate));
+FULLSCREENBUTTON.addEventListener("click", toggleFullScreen);
 
 PROGRESS.addEventListener("click", scrub);
 PROGRESS.addEventListener("mousedown", () => mouseDown = true);
